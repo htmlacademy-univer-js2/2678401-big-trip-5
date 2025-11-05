7161217cd0097f703a0030420918a2743479519a
Egor Sazhin <sazhin2003.egor@gmail.com>
2025-11-05 19:29:25 +0500

    module1-task3 - Время разбивать камни

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import NewTaskButtonView from './view/new-task-button-view';
+import FilterView from './view/filter-view';
+import {render} from './render.js';
+import BoardPresenter from './presenter/board-presenter';
+
+const siteMainElement = document.querySelector('.main');
+const siteHeaderElement = siteMainElement.querySelector('.main__control');
+const boardPresenter = new BoardPresenter({boardContainer: siteMainElement});
+
+render(new NewTaskButtonView(), siteHeaderElement);
+render(new FilterView(), siteMainElement);
+
+boardPresenter.init();
